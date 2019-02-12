22bdd1d01699f87bc73a7ada4e36d3258ef618a4
Grayson Null <graysonnull@me.com>
2019-02-11 17:10:17 -0800

    add a few more base styles to handle things in markdown and overall polish of styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './client/index.css';
+import './client/App.css';
+import './client/App.mobile.css';
 import App from './client/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
